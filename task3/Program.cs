6065cf52fd209a3eec439bbd819efc1dd59a3ddf
Victor <vityacov@gmail.com>
2024-03-13 22:27:05 +0300

    The End

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+int[] numbers = new int[] { 1, 56, 9, 4, 2, 5, 31, 67, 9, 105, 22, 19 };
+
+void PrintArray(int[] array, int index)
+{
+    if (index >= array.Length)
+    {
+        return;
+    }
+    else
+    {
+        PrintArray(array, index + 1);
+        Console.Write(array[index] + " ");
+    }
+}
+
+PrintArray(numbers,0);
